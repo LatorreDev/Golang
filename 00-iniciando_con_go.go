652d23148b42e5706a1f4c9f0a658e51b8309d7c
David Latorre <david@latorredev.com>
2020-06-29 22:45:40 -0500

    [ADD] anadido hola mundo en go, ejecutable y codigo fuente

---
+++ 00-iniciando_con_go.go
@@ -0,0 +1,39 @@
+package main
+
+/*
+
+Caracteristicas
+
+Go es un lenguaje desarrolado por Google
+Tiene una sintaxis clara y estandarizada
+El compilador fuerza a usar buenas practicas
+El compilador es MUY rapido
+Soporte por concurrencia facil de manejar
+Facil de levantar un servidor web
+Performance Alto
+Facil de aprender
+Facil de subir a produccion
+
+------------------------------
+
+Para que usar Go?
+
+Command Line Tools
+
+Infraestructura en el Backend
+
+Herramientas internas de software
+
+Servicios Web
+
+Herramientas de automatizacion
+
+*/
+
+// Hello world in go
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
