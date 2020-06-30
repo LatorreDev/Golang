02244e3bcb32431298c94bde672e92104a5e895c
David Latorre <david@latorredev.com>
2020-06-29 22:59:51 -0500

    [ADD] anadido variables en go, ejecutable y codigo fuente

---
+++ 01-variables_en_go.go
@@ -0,0 +1,20 @@
+package main
+
+//Golang es fuertemente tipado como C
+import "fmt"
+
+func main() {
+	/*
+
+	   var num1,num2,num3 int // Variables declaradas tipo int
+	   var cadena string // Tipo string
+	   var booleano bool // Tipo Bool
+
+	   Las variables siempre se tienen que utilizar, si no
+	    El compilador no las deja usar
+	*/
+
+	num4 := 23 // Declarar una variable con su tipo y su valor en una sola linea
+
+	fmt.Println(num4)
+}
